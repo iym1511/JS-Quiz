bdda86c53a5becb522f0f05ad4cb02012cb33158
문일윤 <iym1511@naver.com>
2023-08-16 18:27:59 +0900

    REFACTOR 49번 최댓값 구하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Quiz48 from './QuizFolder/Quiz48';
+import Quiz49 from './QuizFolder/Quiz49';
 
 function App() {
   return (
     <div className="App">
-      <Quiz48 />
+      <Quiz49 />
     </div>
   );
 }
---
+++ src/QuizFolder/Quiz49.jsx
@@ -0,0 +1,35 @@
+// # 문제49 : 최댓값 구하기
+
+// 순서가 없는 10개의 숫자가 공백으로 구분되어 주어진다. 주어진 숫자들 중 최댓값을 반환하라.
+
+const Quiz49 = () => {
+
+  // 내 코드
+  let arrays = []
+
+  for(let i = 0; i < 10; i++){
+    const num = prompt("순서가 없는 10개의 숫자 입력")
+    arrays.push(Number(num))
+  }
+  console.log(Math.max(...arrays));
+
+
+  // 정답
+  let numbers = prompt('10개의 숫자를 입력하세요').split(' ').map((n) => {
+    console.log(parseInt(n, 10))
+    return parseInt(n, 10);
+  });
+  numbers.sort((a, b) => {
+    return b-a;
+  });
+  
+  console.log(numbers[0]);
+
+  return (  
+    <div>
+
+    </div>
+  );
+}
+ 
+export default Quiz49;
