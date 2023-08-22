c2da33cdaadb1b2030f14532b3d46c20d0e183a9
문일윤 <iym1511@naver.com>
2023-08-22 21:01:50 +0900

    63 줄임말

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Quiz61 from './QuizFolder/Quiz61';
-import Quiz62 from './QuizFolder/Quiz62';
+import Quiz63 from './QuizFolder/Quiz63';
 
 function App() {
   return (
     <div className="App">
-      <Quiz62 />
+      <Quiz63 />
     </div>
   );
 }
---
+++ src/QuizFolder/Quiz63.jsx
@@ -0,0 +1,17 @@
+const Quiz63 = () => {
+
+  const text = prompt('텍스트를 입력하시오.').split(" ");
+  
+  for(let i = 0; i<text.length; i++){
+    console.log(text[i].slice(0,1))
+  }
+
+  
+
+  return (  
+    <>
+    </>
+  );
+}
+ 
+export default Quiz63;
