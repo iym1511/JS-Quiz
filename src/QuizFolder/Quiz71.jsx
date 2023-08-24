d05f5db516d5b086b5aeed90996ec6a7419b7d47
문일윤 <iym1511@naver.com>
2023-08-24 22:45:55 +0900

    71 깊이 우선 순회(전위 순회 - Preorder) 정답 수정

--- src/QuizFolder/Quiz71.jsx
+++ src/QuizFolder/Quiz71.jsx
@@ -1,16 +1,18 @@
+import { useEffect } from "react";
+
 const Quiz71 = () => {
   // 데이터
   const graph = {
-    'A': ['E', 'C', 'B'],
+    'A': ['E', 'B', 'C'],
     'B': ['A'],
     'C': ['A'],
     'D': ['E','F'],
-    'E': ['D', 'A'],
+    'E': ['A', 'D'],
     'F': ['D'],
   };
 
   function dfs(graph, start) {
-    // 
+
     let visited = [];
     // 처음값 E // 4. [D,A]
     let stack = [start];
@@ -21,11 +23,12 @@ const Quiz71 = () => {
       if (!visited.includes(n)) {
         // 1. E push, // 6. A 푸쉬
         visited.push(n);
-        // 2. E : [D,A] 에서 E가 없는문자 필터
+        // 2. E : [D,A] 에서 D A가 없는문자 필터
         let sub = graph[n].filter((x) => !visited.includes(x));
         for (let i of sub) {
           // 3. D,A 푸시
           stack.push(i);
+          
         }
       }
     }
@@ -33,10 +36,11 @@ const Quiz71 = () => {
   }
 
   // 6번까지 반복해서 결과 E A B C D F
-
+  // 하지만 E D F A C D 로 나와야함
   console.log(dfs(graph, "E"));
 
-  return <></>;
+  return <>
+  </>;
 };
 
 export default Quiz71;
