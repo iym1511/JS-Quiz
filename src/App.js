53c32d0eb927398e16edc1009f463fab1ef4812d
문일윤 <iym1511@naver.com>
2023-09-27 14:06:22 +0900

    주사위게임3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Solution17 from './Programmers/Solution17';
+import Solution18 from './Programmers/Solution18';
 
 function App() {
   return (
     <div className="App">
       {/* <Quizs /> */}
-      <Solution17 />
+      <Solution18 />
     </div>
   );
 }
