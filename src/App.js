dcdb218dd876e68ce9d8010a4129ed9c539c862e
문일윤 <iym1511@naver.com>
2023-10-02 17:21:19 +0900

    문자열 앞의 n 글자

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Solution24 from './Programmers/Solution24';
 import Solution25 from './Programmers/Solution25';
+import Solution26 from './Programmers/Solution26';
 
 function App() {
   return (
     <div className="App">
       {/* <Quizs /> */}
-      <Solution25/>
+      <Solution26/>
     </div>
   );
 }
--- src/Programmers/Solution26.jsx
+++ src/Programmers/Solution26.jsx
@@ -1,9 +1,21 @@
 const Solution26 = () => {
+
+  let st = "ProgrammerS123";
+  let n = 11;
+
+  function solution(st, n) {
+    let answer = '';
+    answer = st.slice(0, n);
+    return answer;
+  }
+
+  console.log(solution(st,n))
+
   return (  
     <div>
-      
+
     </div>
   );
 }
- 
+
 export default Solution26;
