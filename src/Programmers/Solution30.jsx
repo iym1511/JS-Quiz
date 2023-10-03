1a1288f37277446782b8650aaa865feb5f3b23ef
문일윤 <iym1511@naver.com>
2023-10-04 01:26:56 +0900

    test upload

---
+++ src/Programmers/Solution30.jsx
@@ -0,0 +1,16 @@
+const Solution30 = () => {
+  const survey = ["AN", "CF", "MJ", "RT", "NA"];
+  const choices = [5, 3, 2, 7, 5];
+
+  function solution(survey, choices) {
+    var answer = "";
+    
+    return answer;
+  }
+
+  console.log(solution(survey, choices));
+
+  return <div></div>;
+};
+
+export default Solution30;
