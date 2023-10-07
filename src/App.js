1e31084779ede25b39472cfb46e9dc82adf878e4
문일윤 <iym1511@naver.com>
2023-10-07 17:48:10 +0900

    음양 더하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Solution36 from './Programmers/Solution36';
+import Solution37 from './Programmers/Solution37';
 
 function App() {
   return (
     <div className="App">
       {/* <Quizs /> */}
-      <Solution36/>
+      <Solution37/>
     </div>
   );
 }
---
+++ src/Programmers/Solution37.jsx
@@ -0,0 +1,25 @@
+const Solution37 = () => {
+
+  let absolutes = [4, 7, 12];
+  let signs = [true, false, true];
+
+  function solution(absolutes, signs) {
+    var answer = 0;
+
+    for(let i = 0; i<absolutes.length; i++){
+      if(signs[i]){
+        answer += absolutes[i]
+      }else{
+        answer -= absolutes[i]
+      }
+    }
+
+    return answer;
+  }
+
+  console.log(solution(absolutes, signs));
+
+  return <div></div>;
+};
+
+export default Solution37;
