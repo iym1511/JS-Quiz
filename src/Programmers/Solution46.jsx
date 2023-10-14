6a516c99bae10ab0f2ded7bda1370937ca52f169
문일윤 <iym1511@naver.com>
2023-10-14 22:52:25 +0900

    가장 가까운 같은 글자

--- src/Programmers/Solution46.jsx
+++ src/Programmers/Solution46.jsx
@@ -24,24 +24,26 @@ const Solution46 = () => {
 
   //   return answer;
   // }
-  let asd = {'a':0};
-  console.log(asd['a'])
+
+  // 객체 사용 예시
+  let asd = {};
+  console.log(asd['b'] = 0);
+  console.log(asd['b'] = 2)
+  console.log(asd)
 
   const s = "banana";
-
+  // 다른사람 코드
   function solution(s) {
     let answer = [];
-    let alpabet = {}; // b,a,n
+    let alpabet = {};
 
     s.split("").forEach((a, i) => {
       if (alpabet[a] >= 0) {
         answer.push(i - alpabet[a]); // 7. 3 - 1 = 2 / 9. 4 - 2 = 2 / 11. 5 - 3 = 2
-        console.log(i - alpabet[a])
       } else {
         answer.push(-1); // 1. -1 / 3. -1 / 5. -1
       }
       alpabet[a] = i; // 2. b : 0 / 4. a : 1 / 6. n : 2 / 8. {b : 0, a : 3, n : 2} / 10. {b:0,a:3,n:4} / 12. {b:0,a:5,n:4}
-      console.log(alpabet)
     });
 
     return answer;
