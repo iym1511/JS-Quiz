5185dc699b89562985ee16ee77453f899daa6bbb
문일윤 <iym1511@naver.com>
2023-10-22 15:21:27 +0900

    예산

--- src/Programmers/Solution53.jsx
+++ src/Programmers/Solution53.jsx
@@ -4,13 +4,16 @@ const Solution53 = () => {
 
   function solution(d, budget) {
 
+    // 💡 map이나 forEach로 했을때는 true,false값이 반환되어서 값이 나오진 않는다.
+    
     let copy = d.sort((a, b) => a - b).filter((a) => {
       // 9 랑 배열 순서대로 뺏을때 0보다 크거나 같은것일때 실행
       if(budget - a >= 0){
         budget -= a // 9랑 배열을 계속 빼줌 
-        return true
+        console.log(budget)
+        return true // 해당 원소가 예산 내에서 선택 가능한 경우
       }
-      return false
+      return false // 해당 원소가 예산 내에서 선택 불가능한 경우
     })
 
     return copy.length;
