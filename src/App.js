14e1cfc5a6382a6862ca44f14072315ca3c5d200
문일윤 <iym1511@naver.com>
2023-10-23 21:41:37 +0900

    어려움

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import Solution51 from './Programmers/\bSolution51';
-import Solution50 from './Programmers/Solution50';
-import Solution52 from './Programmers/Solution52';
-import Solution53 from './Programmers/Solution53';
-import Solution54 from './Programmers/Solution54';
-import Solution55 from './Programmers/Solution55';
-import Solution56 from './Programmers/Solution56';
+import Solution57 from './Programmers/Solution57';
 
 function App() {
   return (
     <div className="App">
       {/* <Quizs /> */}
-      <Solution56/>
+      <Solution57/>
     </div>
   );
 }
--- src/Programmers/Solution57.jsx
+++ src/Programmers/Solution57.jsx
@@ -1,4 +1,43 @@
 const Solution57 = () => {
+
+  const n = 10;
+
+  function solution(n) {
+    let numbers = []
+    let answer = []
+    // 숫자의 제곱근을 반환
+    const root = Math.sqrt(n);
+
+    // 배열 초기설정
+    for (let a = 0; a <= n; a++) {
+      numbers.push(true);
+    }
+
+    // 0과 1은 소수가 아니므로, 해당 인덱스 값을 false로 변경합니다.
+    numbers[0] = false
+    numbers[1] = false
+
+
+    // 합성수는 false로 교체
+    for (let i = 2; i < root; i++) {
+        if (numbers[i]) {
+            // 합성수는 기준수의 배수로 찾는다.
+            // 시작을 기준수의 제곱으로 한다. (본인 수 제외, 제곱 값 이전의 수는 기준수보다 작은 소수가 시행될 때 이미 걸러짐)
+            // 판단해야 할 수는 기준수의 배수이므로, 기준 수를 더해가며 건너 뛴다.
+
+            // i가 2일때 4번 반복, i가3일때 1번반복 총 5번반복
+            for (let j = i * i; j <= n; j += i) {
+                numbers[j] = false;
+            }
+        }
+    }
+
+    answer = numbers.filter(num => num === true)
+    return answer
+}
+
+  console.log(solution(n));
+
   return (  
     <div>
 
