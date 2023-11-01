1d4bc93da33467d5fa39cc14ff9f77cd73f55bb9
문일윤 <iym1511@naver.com>
2023-11-01 15:51:25 +0900

    수박수박...

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Solution66 from './Programmers/Solution66';
 import Solution67 from './Programmers/Solution67';
 import Solution68 from './Programmers/Solution68';
+import Solution69 from './Programmers/Solution69';
 
 function App() {
   return (
     <div className="App">
       {/* <Quizs /> */}
-      <Solution68/>
+      <Solution69/>
     </div>
   );
 }
---
+++ src/Programmers/Solution69.jsx
@@ -0,0 +1,29 @@
+const Solution69 = () => {
+
+  const n = 4
+
+function solution(n) {
+    let answer = '';
+    for(let i = 1; i<=n; i++){
+      if(i % 2 == 0){
+        answer += '박';
+      }
+      if(i % 2 != 0){
+        answer +='수'
+      }
+    }
+    return answer;
+}
+
+console.log(solution(n))
+// 아래 함수를 사용해도 가능
+const waterMelon = n => "수박".repeat(n).slice(0,n);
+
+  return (  
+    <div>
+
+    </div>
+  );
+}
+
+export default Solution69;
