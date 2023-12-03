4cd16c98ea88fa97a9d64aaeb29b5ee00daf8090
문일윤 <iym1511@naver.com>
2023-12-03 16:37:19 +0900

    카펫펫

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import loadable from '@loadable/component';
 import './App.css';
-import Solution94 from './Programmers/Solution94';
-import Solution95 from './Programmers/Solution95';
-import Solution96 from './Programmers/Solution96';
-import Solution97 from './Programmers/Solution97';
-import Solution98 from './Programmers/Solution98';
-import Solution99 from './Programmers/Solution99';
-import Solution100 from './Programmers/Solution100';
-import Solution101 from './Programmers/\bSolution101';
-import Solution102 from './Programmers/Solution102';
-import Solution103 from './Programmers/Solution103';
+import Solution104 from './Programmers/Solution104';
 
 // const Test = loadable(() => import('./Programmers/Solution93'));
 
   function App() {
   return (
     <div className="App">
-      <Solution103/>
+      <Solution104/>
     </div>
   );
 }
---
+++ src/Programmers/Solution104.jsx
@@ -0,0 +1,46 @@
+// 카펫
+
+const Solution104 = () => {
+  // const brown = 10;
+  // const yellow = 2;
+  const brown = 8;
+  const yellow = 1;
+  // const brown = 24;
+  // const yellow = 24;
+
+  // const solution = (brown, yellow) => {
+  //   let answer = [];
+  //   let sum = brown+yellow;
+
+  //   for(let i = 1; i <= sum; i++){
+  //     for(let j = 1; j<=sum; j++){
+  //       if(i * j == sum && j <= i && answer.length < 2){
+  //         answer.push(i);
+  //         answer.push(j);
+  //         break;
+  //       }
+  //     }
+  //   }
+
+  //   return answer;
+  // }
+
+  function solution(brown, yellow) {
+    var answer = [];
+
+    for (var i = 3; i <= (brown + yellow) / i; i++) {
+      var x = Math.floor((brown + yellow) / i); // 1. 48/3 = 16 / 3. 48/4 = 12 / 5. 48/5 = 9 / 7. 48/6 = 8
+      if ((x - 2) * (i - 2) === yellow) { // 2. 16 * 1 === yellow / 4. 10 * 2 === yellow / 6. 7 * 3 === yellow / 8. 6 * 4 === yellow
+        break;
+      }
+    }
+
+    return [x, i];
+  }
+
+  console.log(solution(brown, yellow));
+
+  return <div></div>;
+};
+
+export default Solution104;
