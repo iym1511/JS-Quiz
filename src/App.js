6db0d9f32fab3788dbd980d9dac46114dbad75d8
문일윤 <iym1511@naver.com>
2023-12-04 20:38:27 +0900

    귤 고르기

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import loadable from '@loadable/component';
 import './App.css';
 import Solution104 from './Programmers/Solution104';
 import Solution105 from './Programmers/Solution105';
+import Solution106 from './Programmers/Solution106';
 
 // const Test = loadable(() => import('./Programmers/Solution93'));
 
   function App() {
   return (
     <div className="App">
-      <Solution105/>
+      <Solution106/>
     </div>
   );
 }
---
+++ src/Programmers/Solution106.jsx
@@ -0,0 +1,39 @@
+const Solution106 = () => {
+
+  const k = 6;
+  const tangerine = [1, 3, 2, 5, 4, 5, 2, 3];
+
+  const solution = (k, tangerine) => {
+    let answer = 0;
+    let obj = {}
+    tangerine.map(e => {
+      if(!obj[e]){
+          obj[e] = 1
+      }else{
+          obj[e] += 1;
+      }
+    }) 
+
+    // 객체의 value 내림차순
+    let values = Object.values(obj).sort((a,b)=>b-a)
+
+    for(const value of values){
+        answer++
+        // 6-2=4, 4-2=2, 2 <= 2 break // 결과 answer=3
+        if(k-value<=0) break
+        else k-= value
+    }
+
+    return answer;
+  }
+
+  console.log(solution(k, tangerine));
+
+  return (  
+    <div>
+
+    </div>
+  );
+}
+
+export default Solution106;
