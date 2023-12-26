2642236267c6e527e0a4a30ca26f16a72e7a86b0
문일윤 <iym1511@naver.com>
2023-12-26 16:04:44 +0900

    숫자만 추출

--- src/JSalgorithm/문자열 탐색/Sol3.jsx
+++ src/JSalgorithm/문자열 탐색/Sol3.jsx
@@ -1,11 +1,25 @@
 // 숫자만 추출
 const Sol3 = () => {
 
-  const solution = () => {
+  const str = "g0en2T0s8eSoft";
 
+  const solution = (s) => {
+    let answer = "";
+
+    // s = s.toLowerCase().replace(/[^0-9]/g,"")
+    // answer = parseInt(s);
+
+    // 강의내용 방법
+    for(let x of s){
+      if(!isNaN(x)){
+        answer += x;
+      }
+    }
+
+    return parseInt(answer);
   }
 
-  console.log(solution());
+  console.log(solution(str));
 
   return (  
     <div>
