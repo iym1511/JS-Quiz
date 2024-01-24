e69e18a4a15f383fc11ef95e7a0b9896d19f1eb4
문일윤 <iym1511@naver.com>
2024-01-24 13:53:09 +0900

    바둑이 승차(이진트리DFS)

--- src/JSalgorithm/재귀함수와 완전탐색(DFS:깊이우선탐색)/Sol6.jsx
+++ src/JSalgorithm/재귀함수와 완전탐색(DFS:깊이우선탐색)/Sol6.jsx
@@ -1,3 +1,4 @@
+// 바둑이 승차(이진트리DFS)
 const Sol6 = () => {
   const arr = [81, 58, 42, 33, 61];
 
@@ -10,6 +11,7 @@ const Sol6 = () => {
       if (sum > max) return;
       if (L === n) {
         if (sum < max) {
+          console.log(sum)
           answer = Math.max(answer, sum);
         }
       } else {
@@ -21,6 +23,9 @@ const Sol6 = () => {
 
     return answer;
   };
+
+  // 17줄 :  1.DFS(0+1, 0+81) / 2.DFS(1+1, 81+58) / 3.DFS(2+1, 139+42) / 4.DFS(3+1, 181+33) / 5.DFS(4+1, 214+61) 백트래킹
+  // 18줄 :  6.DFS(4+1, 214) 백트래킹 / 7.DFS(3+1, 181+33) DFS(3+1, 181) / 8.DFS(4+1, 181+61)(결과값) 백트래킹  / 9.DFS(2+1, 139+42) DFS(2+1, 139)실행 / 10.DFS(3+1, 139+33) / 11.DFS(4+1, 172+61) 백트래킹 이후 반복
 
   console.log(solution(arr));
 
