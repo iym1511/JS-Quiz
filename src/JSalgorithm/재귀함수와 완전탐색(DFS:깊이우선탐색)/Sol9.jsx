177dac42d65c56175a3d8762876bc0d2f6dde7b6
문일윤 <iym1511@naver.com>
2024-01-26 14:55:49 +0900

    동전교환

--- src/JSalgorithm/재귀함수와 완전탐색(DFS:깊이우선탐색)/Sol9.jsx
+++ src/JSalgorithm/재귀함수와 완전탐색(DFS:깊이우선탐색)/Sol9.jsx
@@ -12,12 +12,13 @@ const Sol9 = () => {
       if(L >= answer) return;
 
       if(sum === m){
-        console.log(L,sum)
+        // console.log(L+1,sum)
         answer = Math.min(answer, L)
       }else{
         for(let i=0; i<n; i++){
-          // console.log(L+1, sum+arr[i])
+          console.log(L+1, sum+arr[i], `arr[i] : ${arr[i]}`) 
           DFS(L+1, sum+arr[i]) // 1.DFS(0+1, 0+1) / 2.DFS(1+1, 1+2) / 3.DFS(2+1, 3+5)
+          // 백트래킹 1.(14+1, 14+2 / 14+1, 14+5) 14끝 2.(13+1, 13+2 / 13+1, 13+5) 13끝백트래킹 3.(12+1, 12+2{14라서 for문다시 ->} / 13+1, 14+1)백트래킹 4.(13+1, 14+2백트래킹 / 13+1, 14+5백트래킹) 5.(12+1, 12+5백트래킹 /11+1, 11+2) 6.(12+1, 13+1 / 13+1, 14+1백트래킹) 추후 이대로반복
         }
       }
     }
