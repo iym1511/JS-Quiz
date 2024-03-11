e0997150fb6cb3c20fa620202b146852689ff96b
문일윤 <iym1511@naver.com>
2024-03-11 14:12:58 +0900

    수들의 조합(복습)

--- src/JSalgorithm/그래프와 탐색(DFS, BFS:너비우선탐색)/Sol3.jsx
+++ src/JSalgorithm/그래프와 탐색(DFS, BFS:너비우선탐색)/Sol3.jsx
@@ -113,7 +113,7 @@ const Sol3 = () => {
 
     return answer;
   };
-  console.log(solution5(3, 2));
+  console.log("solution5 : ", solution5(3, 2));
 
   const solution6 = (arr, money) => {
     // 기본값이 0 인데 최소값 구할때 사용
@@ -170,7 +170,7 @@ const Sol3 = () => {
     let answer;
     const DFS = (n) => {
       if (n === 1) return 1;
-      else return answer = n * DFS(n - 1);
+      else return (answer = n * DFS(n - 1));
     };
     answer = DFS(num);
 
@@ -180,101 +180,123 @@ const Sol3 = () => {
 
   const solution9 = (n, r) => {
     let answer;
-    let dy = Array.from(Array(7), ()=>Array(7).fill(0));
-    console.log(dy)
-    const DFS = (n,r) => {
-      if(dy[n][r] > 0){
-        console.log("0이 1된거",n,r)
+    let dy = Array.from(Array(7), () => Array(7).fill(0));
+    console.log(dy);
+    const DFS = (n, r) => {
+      if (dy[n][r] > 0) {
+        console.log("0이 1된거", n, r);
         return dy[n][r];
-      } 
-      if(n === r || r === 0) return 1;
+      }
+      if (n === r || r === 0) return 1;
       else {
-        console.log(DFS(n-1,r-1),DFS(n-1,r));
-        return dy[n][r] = DFS(n-1, r-1)+DFS(n-1,r); 
-        // dy[5][3] = 4,2(6가지 경우의 수) 
+        console.log(DFS(n - 1, r - 1), DFS(n - 1, r));
+        return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+        // dy[5][3] = 4,2(6가지 경우의 수)
         // dy[4][2] = 3,1(3가지 경우의 수)
         // dy[3][1] = 2,0(return 1)
         // 백트래킹) dy[3][1] = 2,0(return 1) + 2,1(2가지 경우의 수)  = 3
-        // 백트래킹) dy[2][1] = 1,0(return 1) 
+        // 백트래킹) dy[2][1] = 1,0(return 1)
         // 백트래킹) dy[2][1] = 1.0(return 1) + 1,1(1가지 경우의 수) = 2
         // 백트래킹) dy[4][2] = 3,1(3가지 경우의 수) + 3,2(3가지 경우의 수) = 6
         // dy[3][2] = 2,1(2가지 경우의 수) + 2,2(1가지 경우의 수) = 3
         // 나머지 해봤던거라 if문에서 걸림
         // 백트래킹) dy[5][3] = 4,2(6가지 경우의 수) + 4,3(4가지 경우의 수) = 10
         // dy[4][3] = 3,2(3가지 경우의 수) + 3,3(return 1) = 4
-        
-      } 
-    }
-    answer = DFS(n,r)
+      }
+    };
+    answer = DFS(n, r);
 
     return answer;
-  }
-  console.log(solution9(5,3))
+  };
+  console.log(solution9(5, 3));
 
   const solution10 = (n, f) => {
     let answer = [];
-    // 
-    let dy = Array.from(Array(11), () => Array(11).fill(0))
+    //
+    let dy = Array.from(Array(11), () => Array(11).fill(0));
     // 순열 돌리기 위한 체크배열
-    let ch = Array.from({length : n+1}, () => 0);
+    let ch = Array.from({ length: n + 1 }, () => 0);
     // 순열 저장
-    let p = Array.from({length : n}, () => 0);
+    let p = Array.from({ length: n }, () => 0);
     // 조합수 저장 1,3,3,1
-    let b = Array.from({length : n}, () => 0);
+    let b = Array.from({ length: n }, () => 0);
     let flag = 0;
 
     const combi = (n, r) => {
-      if(dy[n][r] > 0)  return dy[n][r];
-      if(n === r || r === 0) return 1;
-      else return dy[n][r] = combi(n-1, r-1) + combi(n-1, r);
-
-    }
+      if (dy[n][r] > 0) return dy[n][r];
+      if (n === r || r === 0) return 1;
+      else return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+    };
     const DFS = (L, sum) => {
-      if(flag) return
-      if(L === n && sum === f){
+      if (flag) return;
+      if (L === n && sum === f) {
         answer.push(p.slice());
-        flag = 1
-      }else{
-        for(let i = 1; i<=n; i++){
-          if(ch[i] === 0){
-            ch[i] = 1
-            p[L] = i
-            console.log(L,i)
+        flag = 1;
+      } else {
+        for (let i = 1; i <= n; i++) {
+          if (ch[i] === 0) {
+            ch[i] = 1;
+            p[L] = i;
+            console.log(L, i);
             // console.log(i); // (1)1 1 / (2)2 3 / (3)3 3 / (4)4 1 : 1+6+9
-            DFS(L+1, sum+(b[L]*p[L]));
-            console.log(p,L,sum,i)
-            ch[i] = 0
+            DFS(L + 1, sum + b[L] * p[L]);
+            console.log(p, L, sum, i);
+            ch[i] = 0;
           }
         }
       }
+    };
+    for (let i = 0; i < n; i++) {
+      b[i] = combi(n - 1, i);
     }
-    for(let i = 0; i < n; i++){
-      b[i] = combi(n-1, i);
-    }
-    DFS(0,0)
+    DFS(0, 0);
     return answer;
-  }
-  console.log(solution10(4,16))
+  };
+  console.log(solution10(4, 16));
 
-  const solution11 = (n,m) => {
+  const solution11 = (n, m) => {
     let answer = [];
-    let tmp = Array.from({length:m}, () => 0);
+    let tmp = Array.from({ length: m }, () => 0);
 
-    const DFS = (L,S) => {
-      if(L===m){
+    const DFS = (L, S) => {
+      if (L === m) {
         answer.push(tmp.slice());
-      }else {
-        for(let i = S; i<=n; i++) {
-            tmp[L] = i
-            DFS(L+1, i+1); // for문을 이어가기위해 S가 아닌 i에 1을 더함
+      } else {
+        for (let i = S; i <= n; i++) {
+          tmp[L] = i;
+          DFS(L + 1, i + 1); // for문을 이어가기위해 S가 아닌 i에 1을 더함
         }
       }
-    }
-    DFS(0,1);
+    };
+    DFS(0, 1);
 
     return answer;
-  }
-  console.log(solution11(4,2));
+  };
+  console.log(solution11(4, 2));
+
+  const solution12 = (arr, k, m) => {
+    let answer = 0;
+    let tmp = Array.from({ length: m }, () => 0);
+    let len = arr.length;
+
+    const DFS = (L, s,sum) => {
+      if (L === m) {
+        if (sum % k === 0) {
+          console.log(sum, tmp);
+          answer++;
+        }
+      } else {
+        for (let i = s; i < len; i++) {
+          tmp[L] = arr[i];
+          DFS(L + 1, i + 1, sum+arr[i]);
+        }
+      }
+    };
+    DFS(0, 0, 0);
+
+    return answer;
+  };
+  console.log(solution12([2, 4, 5, 8, 12], 6, 3));
 
   return <div></div>;
 };
